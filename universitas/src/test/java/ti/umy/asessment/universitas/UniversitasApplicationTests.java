edc0a9f799a1e751714942118d5acb56ed82b518
ffadelm <faysalfadelm0107@gmail.com>
2021-12-22 22:09:05 +0700

    second commit

---
+++ universitas/src/test/java/ti/umy/asessment/universitas/UniversitasApplicationTests.java
@@ -0,0 +1,13 @@
+package ti.umy.asessment.universitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
